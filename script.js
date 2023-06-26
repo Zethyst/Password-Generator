b879b75e24783c19e4da1969ac823a4a62acb310
Ekansh <zethyst@protonmail.com>
2023-06-27 02:29:34 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const lengthSlider=document.querySelector(".pass-length input"),
+options=document.querySelector(".option input"),
+copyIcon=document.querySelector(".copy"),
+passwordInput=document.querySelector("#pwd"),
+passIndicator=document.querySelector(".pass-indicator"),
+generateBtn=document.querySelector(".button button");
+const pupperCase = document.getElementById('uppercase');
+const plowerCase = document.getElementById('lowercase');
+const pNumber = document.getElementById('numbers');
+const pSymbol = document.getElementById('symbols');
+
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*=-_";
+
+submit.addEventListener('click', ()=>{
+    let initPwd = "";
+    (pupperCase.checked) ? initPwd += uppercase : "";
+    (plowerCase.checked) ? initPwd += lowercase : "";
+    (pNumber.checked) ? initPwd += number : "";
+    (pSymbol.checked) ? initPwd += symbol : "";
+
+    passwordInput.value = generatePwd(lengthSlider.value,initPwd);
+});
+
+function generatePwd(len, initPwd){
+    let pass = "";
+    for (let i=0;i<len;i++){
+        pass += initPwd.charAt(Math.floor(Math.random() * initPwd.length));
+    }
+    updatePassIndicator();
+    return pass;
+}
+
+const updatePassIndicator = ()=>{
+    passIndicator.id=
+        lengthSlider.value<=4
+        ?"weak"
+        : lengthSlider.value <=10
+        ? "medium"
+        : lengthSlider.value <=15
+        ? "strong"
+        : "superstrong";
+};
+
+const updateSlider = () =>{
+    document.querySelector(".pass-length span").innerText=lengthSlider.value;
+}
+updateSlider();
+
+const copyPassword = () =>{
+    navigator.clipboard.writeText(passwordInput.value);
+    Icon=document.querySelector(".copy i"),
+    Icon.innerText="check";
+    Icon.style.color="rgb(33, 163, 33)";
+    setTimeout(()=>{
+        Icon.innerText="content_copy";
+        Icon.style.color="rgb(41, 41, 41)";
+    },1500);
+};
+
+copyIcon.addEventListener("click",copyPassword);
+lengthSlider.addEventListener("input",updateSlider);
